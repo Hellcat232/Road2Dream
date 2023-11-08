793a861409639c9b66a3c3d5f5f61dddf5a8b699
Timoshchyk <timosuko202@gmail.com>
2023-11-08 19:37:09 +0200

    Merge branch 'main' of https://github.com/Hellcat232/Road2Dream

--- src/main.js
+++ src/main.js
@@ -1,18 +1,15 @@
 (() => {
-    const menu = document.querySelector('.header-menu-mob');
-    const backdrop = document.querySelector('.backdrop');
-const menuLinks = document.querySelector('.unlock-menu');
-const navLogo = document.querySelector('.menu-close-btn');
+  const menu = document.querySelector('.header-menu-mob');
+  const backdrop = document.querySelector('.backdrop');
+  const menuLinks = document.querySelector('.unlock-menu');
+  const navLogo = document.querySelector('.menu-close-btn');
 
-
-// Display Mobile Menu
-const mobileMenu = () => {
+  // Display Mobile Menu
+  const mobileMenu = () => {
     menu.classList.toggle('is-open');
     backdrop.classList.toggle('is-open');
-};
+  };
 
-
-menuLinks.addEventListener('click', mobileMenu);
-navLogo.addEventListener('click', mobileMenu);
-}
-)();
+  menuLinks.addEventListener('click', mobileMenu);
+  navLogo.addEventListener('click', mobileMenu);
+})();
